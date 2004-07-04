9449862d7d87ea34b7127cac19be69841bb9d4db
jlawson <jlawson>
2004-07-04 07:29:37 +0000

    update version to 1.3.0

--- version.h
+++ version.h
@@ -3,6 +3,6 @@
 #define PROG_DESC_LONG      "distributed.net Logfile Visualizer"
 #define PROG_DESC_LONGZ     PROG_DESC_LONG "\0"
 
-#define PROG_VERSION_STR    "1.2.2"
+#define PROG_VERSION_STR    "1.3.0"
 #define PROG_VERSION_STRZ   PROG_VERSION_STR "\0"
 
